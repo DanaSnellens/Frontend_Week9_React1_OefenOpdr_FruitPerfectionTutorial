e47d22fbbf39ad8608f5714a81b5e8c8506a13d6
DanaSnellens <danasnellens@gmail.com>
2023-11-12 11:56:26 +0100

    Klaar

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
 import React from "react";
 import './App.css'
 
+import {ReactComponent as ShoppingCart} from './assets/winkelmandje.svg';
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
+import Product from "./components/Product.jsx";
+
 function App() {
+    function logClick(){
+        console.log("You clicked!")
+    }
     return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Ons verhaal</a></li>
+                    <li><a href="/">Blog</a></li>
+                </ul>
+                <ShoppingCart className="shopping-cart-icon"/>
+            </nav>
+
+        <header>
+            <h1>Fruit perfection</h1>
+            <button type="button" onClick={logClick}>
+                Shop nu
+            </button>
+        </header>
         <main>
+            <Product
+                image={citroenen}
+                title="Citroen"
+                description="Een citroen is voor de meeste mensen te zuur om zo uit
+                de hand te eten. Van citroen kun je het vruchtvlees, het
+                sap en de schil gebruiken. Het sappige, lichtgele zure
+                vruchtvlees versterkt de smaak van ander voedsel."
+            />
 
-            <article className="product">
-                <img
-                    src="https://t3.ftcdn.net/jpg/00/79/23/36/360_F_79233682_rJFWyfzIMtZVRd6fZEWj1hLshobcDm5u.jpg"
-                    alt="Citroen"
-                    />
-                <h2 className="product-name">Citroen</h2>
-                <p className="product-description">
-                    Een citroen is voor de meeste mensen te zuur om zo uit
-                    de hand te eten. Van citroen kun je het vruchtvlees, het
-                    sap en de schil gebruiken. Het sappige, lichtgele zure
-                    vruchtvlees versterkt de smaak van ander voedsel.
-                </p>
-            </article>
-
-            <article className="product">
-                <img
-                    src="https://t3.ftcdn.net/jpg/00/79/23/36/360_F_79233682_rJFWyfzIMtZVRd6fZEWj1hLshobcDm5u.jpg"
-                    alt="Limoen"
-                />
-                <h2 className="product-name">Limoen</h2>
-                <p className="product-description">
-                    Limoen is familie van de citroen en de sinaasappel en behoort
+            <Product
+                image={limoenen}
+                title="Limoen"
+                description="Limoen is familie van de citroen en de sinaasappel en behoort
                     tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en
-                    kleiner dan citroenen. De schil is dun, vrij glad en groen.
-                </p>
-            </article>
+                    kleiner dan citroenen. De schil is dun, vrij glad en groen."
+            />
 
-            <article className="product">
-                <img
-                    src="https://t3.ftcdn.net/jpg/00/79/23/36/360_F_79233682_rJFWyfzIMtZVRd6fZEWj1hLshobcDm5u.jpg"
-                    alt="Ijsblokjes"
-                />
-                <h2 className="product-name">Ijsblokjes</h2>
-                <p className="product-description">
-                    Een ijsblokje of ijsklontje is bevroren water in de vorm van een
+            <Product
+                image={ijsblokjes}
+                title="Ijsblokjes"
+                description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een
                     klein blokje. Het wordt gemaakt in een diepvriezer door water in
-                    een plastic vorm te laten bevriezen.
-                </p>
-            </article>
+                    een plastic vorm te laten bevriezen."
+            />
         </main>
-    )
+        </>
+    );
 }
 
 export default App
